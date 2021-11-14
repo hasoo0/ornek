1454bc476ff04af97aa1859b8fc0fd22336b6af7
hasan huseyin <hkizilay65@gmail.com>
2021-11-14 12:44:15 +0300

    HesapMakinesi degisti. kisi ve kisi test eklendi.

---
+++ src/com/bilgeadam/ornek/Kisi.java
@@ -0,0 +1,76 @@
+package com.bilgeadam.ornek;
+
+public class Kisi {
+
+    private int no;
+    private String ad;
+    private String soyad;
+    private double maas;
+
+    public Kisi() {
+    }
+
+    public Kisi(int no, String ad, String soyad, double maas) {
+        this.no = no;
+        this.ad = ad;
+        this.soyad = soyad;
+        this.maas = maas;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public double getMaas() {
+        return maas;
+    }
+
+    public void setMaas(double maas) {
+        this.maas = maas;
+    }
+    public double yillik_maas_getir()
+    {
+        double yillik_maas = this.maas * 12;
+
+        return yillik_maas;
+
+    }
+
+    public String adsoyadgetir()
+    {
+        return this.ad+" "+this.soyad;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Kisi{" +
+                "no=" + no +
+                ", ad=" + ad +
+                ", soyad=" + soyad +
+                ", maas=" + maas +
+                ",Yillik Maas=" + yillik_maas_getir()+
+                ",Ad Soyad:" + adsoyadgetir() +
+                '}';
+    }
+}
---
+++ src/com/bilgeadam/ornek/KisiTest.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.ornek;
+
+public class KisiTest {
+
+    public static void main(String[] args) {
+        Kisi Kisi = new Kisi(1,"cahit", "kizilay", 8000);
+        System.out.println(Kisi);
+    }
+
+}
